a8262736067fbbccd8f865a14db6b2c512db06c2
sanju6126 <sanjusv1006@gmail.com>
2023-09-27 17:13:12 +0530

    Designed the client side and made the Client side recieve the data from the Server

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/home/Home";
+import Topbar from "./components/topbar/TopBar";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 function App() {
+  const { user } = useContext(Context);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Topbar  />
+        <Routes>
+          <Route exact path="/" element={<Home/>}></Route>
+          <Route path="/posts" element={<Home/>}></Route>
+          <Route path="/register" element={user ? <Home /> : <Register />}></Route>
+          <Route path="/login" element={user ? <Home /> : <Login />}></Route>
+          <Route path="/post/:id" element={<Single />}></Route>
+          <Route path="/write" element={user ? <Write /> : <Login />}></Route>
+          <Route path="/settings" element={user ? <Settings /> : <Login />}></Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
